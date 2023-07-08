c9f243ab46fe55979491525302cc17f081136511
TSIDEX <78680298+TSIDEX@users.noreply.github.com>
2023-07-08 17:27:26 +0500

    Add files via upload

--- TSFIX.lua
+++ TSFIX.lua
@@ -1,15 +1,12 @@
 local SE = require 'samp.events'
 
-
-
-
-function main() --main loop that starts at game launch
-	if not isSampfuncsLoaded() or not isSampLoaded() then return end --this is a samp specified script that uses sampfuncs, so, we have to kill this script if both not loaded
-	while not isSampAvailable do wait(0) end --waiting samp to load, idk why, but..
-	sampfuncsRegisterConsoleCommand("tstest", tstest) --registering sf console command
+function main()
+	if not isSampfuncsLoaded() or not isSampLoaded() then return end
+	while not isSampAvailable do wait(0) end 
+	sampfuncsRegisterConsoleCommand("tstest", tstest) 
 	wait(-1) --wait inf
 end
 
-function SE.onShowDialog(dialogid, style, title, button1, button2, text) -- ¬  ŒÕ≈÷ — –»œ“¿
+function SE.onShowDialog(dialogid, style, title, button1, button2, text) 
 if dialogid == 632 then return false end -- FIXCAR
 end
